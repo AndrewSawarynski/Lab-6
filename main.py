a32d408af4b7fde0b7cfe46e295ab104890548ac
AndrewSawarynski <Andrew4310@outlook.com>
2023-10-24 18:43:39 -0400

    added code

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 #Andrew Sawarynski partnered with Cienna Perez
 
 def main():
-    print("")
+    while True:
+        print("1. To encode")
+        print("2. To decode")
+        print("3. To exit")
+        choice = input("Enter your choice (1 or 2 or 3): ")
+        if choice == "1":
+            password = input("Enter an 8-digit password: ")
+            print("Encoded password is", encode(password))
+        elif choice == "2":
+            value = input("Enter an already encoded 8-digit password: ")
+            print("Decoded password is", decode(password))
+        elif choice == "3":
+            break
+        else:
+            print("Not availible option")
+        print()
 
+def encode(password):
+    final_res = ''
+    for num in password:
+        new_num = str((int(num) + 3) % 10)
+        final_res += new_num
+    return final_res
 
-
-def encode():
-    print("")
+main()
